555c9997ddbaca47cd8ff54358d748e825b792c7
BluebirdLyrase <38246181+BluebirdLyrase@users.noreply.github.com>
2019-12-03 18:55:38 +0700

    14

--- frontend/js/product.js
+++ frontend/js/product.js
@@ -9,7 +9,28 @@ $(function () {
         $("#plist").empty();
         // #12 Get all products and display as a table
         // use $.get
-
+        $.get(url, function (data, status) {
+            if (status == 'success') {
+                $.each(data, function (index, item) {
+                    console.log(index + ' : ' + item['name']);
+                    $('#plist').append(`<tr>
+                                            <td><img src="${item['photo']}"></td>
+                                            <td>${item['serialno']}</td>
+                                            <td>${item['name']}</td>
+                                            <td>${item['category']}</td>
+                                            <td>${item['price']}</td>
+                                            <td><a href="productdetail.html?pid=${item['serialno']}" class="btn btn-primary" style="color:white" role="button">Detail</a></td>
+                                            </tr>`);
+        
+                });
+            }
+        });
+        // <th>Photo</th>
+        // <th>Serial No.</th>
+        // <th>Name</th>
+        // <th>Category</th>
+        // <th>Price</th>
+        // <th>Action</th>
         // ===============================
     }
     
@@ -27,10 +48,10 @@ $(function () {
             price: $("#price").val(),
             photo: $("#photo").val()
         }
-
+        // var obj = JSON.parse(newproduct);
         // #13 Add new products by calling api
         // use $.post
-        $.post(url, function(newproduct, status){
+        $.post(url,newproduct, function(status){
             alert("Data: " + newproduct + "\nStatus: " + status);
           });
         // ===============================
--- frontend/js/productdetail.js
+++ frontend/js/productdetail.js
@@ -10,7 +10,22 @@ $(function () {
     function getData() {
         // #14 Get a selected product and display as a form
         // use $.get
+        // $.get(url,{serialno:pid}, function (data, status) {
+        $.get(url, function (data, status) {
+            if (status == 'success') {
+                $.each(data, function (index, item) {
+                    console.log(index + ' : ' + item['name']);
+                    $('#preview').attr('src',item['photo']);
+                    $('#photo').attr('value',item['photo']);
+                    $('#serialno').attr('value',item['serialno']);
+                    $('#name').attr('value',item['name']);
+                    $('#category').attr('value',item['category']);
+                    $('#price').attr('value',item['price']);
 
+        
+                });
+            }
+        });
         // ===============================
     }
 
@@ -47,7 +62,7 @@ $(function () {
     });
 
     $("#confirmdelete").click(function () {
-        // #15 Get a selected product and go back to product list
+        // #15 Get a delete product and go back to product list 
         // use $.get and winidow.location.href
 
         // ===============================
